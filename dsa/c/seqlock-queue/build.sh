6f9c942bfcbbff2ca42a746563876d484b297a6d
Martin Othamar <martin@othamar.net>
2023-07-03 16:15:13 +0200

    Makefiles

--- dsa/c/seqlock-queue/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-gcc -Wall -Wextra -pedantic -Werror -O3 -g3 -std=c11 -lpthread -o ./bin/spmc main.c && ./bin/spmc
--- dsa/c/spsc/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-gcc -Wall -Wextra -pedantic -Werror -O3 -march=native -std=c11 -lpthread -o ./bin/spsc main.c && ./bin/spsc
