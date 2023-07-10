7148b71ab272537568e16d55ef166de8fd798847
Martin Othamar <martin@othamar.net>
2023-07-11 00:14:25 +0200

    hmm...

--- monte-carlo-sim/csharp/Program.cs
+++ monte-carlo-sim/csharp/Program.cs
@@ -1,7 +1,48 @@
 ﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Runtime.Intrinsics;
+using System.Runtime.Intrinsics.X86;
 using System.Text.Json;
+using System.Xml.XPath;
+using Fast.PRNGs;
 
 Console.WriteLine("Starting simulation");
+
+// {
+//     var poissonVec = Vector256.Create(0.5d);
+//     Vector256<double> productVec = Vector256.Create(0.4, 0.4, 0.9, 0.9);
+
+//     Vector256<double> goals = default;
+
+//     var adder = Vector256.Create(1d);
+//     while (true)
+//     {
+//         var condition = Avx2.CompareGreaterThanOrEqual(productVec, poissonVec);
+//         goals = Avx2.Add(goals, Avx2.And(adder, condition));
+//         productVec = Avx2.Multiply(productVec, Vector256.Create(0.7d));
+
+//         var sub = Avx.Subtract(productVec, poissonVec);
+//         var mask = Avx2.MoveMask(sub);
+//         if (mask == 0x000F)
+//             break;
+//     }
+// }
+
+
+// {
+//     var poissonVec = 0.5d;
+//     var goals = 0d;
+//     var productVec = 0.8d;
+//     var adder = 1d;
+//     var firstResult = goals + adder;
+//     var condition = poissonVec;
+//     condition =
+//         productVec >= condition
+//             ? BitConverter.UInt64BitsToDouble(0xFFFFFFFFFFFFFFFF)
+//             : BitConverter.UInt64BitsToDouble(0x0000000000000000);
+//     firstResult = BitConverter.DoubleToInt64Bits(firstResult) & BitConverter.DoubleToInt64Bits(condition);
+//     Console.WriteLine(firstResult.ToString());
+// }
 
 await using var file = File.OpenRead("../input.json");
 var teams = await JsonSerializer.DeserializeAsync(file, AppJsonSerializerContext.Default.TeamDtoArray);
@@ -11,23 +52,33 @@ if (teams is null || teams.Length == 0)
 
 Console.WriteLine($"Loaded {teams.Length} teams");
 
-const int iterations = 8;
-var elapsed = new TimeSpan[iterations];
-
-var timer = new Stopwatch();
-
-var input = new Input(100_000, teams);
-
-for (int i = 0; i < iterations; i++)
-{
-    timer.Start();
-    Simulation.Run(in input);
-    timer.Stop();
-    elapsed[i] = timer.Elapsed;
-    timer.Reset();
-}
-
-for (int i = 0; i < iterations; i++)
-    Console.WriteLine($"Elapsed: {elapsed[i].TotalMilliseconds:0.000}ms");
+Run(teams);
 
 return 0;
+
+static void Run(TeamDto[] teams)
+{
+    const int iterations = 16;
+    Span<TimeSpan> elapsed = stackalloc TimeSpan[iterations];
+
+    var state = new State(100_000, teams);
+    try
+    {
+        for (int i = 0; i < iterations; i++)
+        {
+            var start = Stopwatch.GetTimestamp();
+            Simulation.Run(ref state);
+            var stop = Stopwatch.GetTimestamp();
+            var duration = Stopwatch.GetElapsedTime(start, stop);
+            elapsed[i] = duration;
+            // state.Reset();
+        }
+    }
+    finally
+    {
+        state.Dispose();
+    }
+
+    for (int i = 0; i < iterations; i++)
+        Console.WriteLine($"Elapsed: {elapsed[i].TotalMilliseconds:0.000}ms");
+}
--- monte-carlo-sim/csharp/Simulation.cs
+++ monte-carlo-sim/csharp/Simulation.cs
@@ -1,113 +1,195 @@
 using System.Diagnostics;
+using System.Numerics;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics;
+using System.Runtime.Intrinsics.X86;
 using Fast.PRNGs;
 
 [module: SkipLocalsInit]
 
-internal readonly struct Input
+unsafe internal struct State : IDisposable
 {
-    public readonly int Simulations;
-    public readonly int TeamCount;
-    public readonly Teams Teams;
+    private const nuint Size = (nuint)PoissonOffset + MatchesOffset + ScoresOffset + Simulation.MaxNumberOfMatches * 2;
+    private const int PoissonOffset = 0;
+    private const int MatchesOffset = PoissonOffset + (sizeof(double) * (Simulation.MaxNumberOfTeams * 2)); // Home and Away poisson per team
+    private const int ScoresOffset = PoissonOffset + MatchesOffset + (Simulation.MaxNumberOfMatches * 2);
 
-    public Input(int simulations, TeamDto[] teams)
+    internal Shishua Rng;
+    internal readonly HashSet<(byte Home, byte Away)> Matchups;
+
+    private readonly void* _ptr;
+    private readonly nuint _ptrSize;
+
+    internal readonly int TeamCount;
+    internal readonly int MatchCount;
+    internal readonly int Simulations;
+
+    private readonly Span<byte> _raw => new(_ptr, (int)_ptrSize);
+
+    internal readonly Span<double> Poisson => new((byte*)_ptr + PoissonOffset, Simulation.MaxNumberOfTeams * 2);
+    internal readonly Span<byte> Matches => new((byte*)_ptr + MatchesOffset, Simulation.MaxNumberOfMatches * 2);
+    internal readonly Span<byte> Scores => new(((byte*)_ptr) + ScoresOffset, Simulation.MaxNumberOfMatches * 2);
+
+    public State(int simulations, TeamDto[] teams)
     {
-        Simulations = simulations;
-        TeamCount = teams.Length;
-        unsafe
+        Debug.Assert(teams.Length <= 32, "We store team indice as ID as a byte, and allocate fixed/static buffers");
+
+        Rng = Shishua.Create();
+        Matchups = new(Simulation.MaxNumberOfMatches);
+        _ptrSize = (nuint)Simulation.NextPow2((int)Size);
+        _ptr = NativeMemory.AlignedAlloc(
+            _ptrSize,
+            (nuint)(1024 * 4) /* 4k page size */
+        );
+        if (_ptr is null)
+            throw new Exception("Couldn't allocate memory");
+
+        // Make sure we touch all memory backed by _ptr, there were a lot of TLB cache misses...
+        // This way, all virtual pages should be mapped to pshycal memory immediately
+        for (int i = 0; i < (int)_ptrSize; i++)
+            *((byte*)_ptr + i) = 0;
+
+        var poisson = Poisson;
+
+        for (int i = 0; i < teams.Length; i++)
         {
-            for (int i = 0; i < teams.Length; i++)
-            {
-                Teams.PoissonLimit[i] = double.Exp(-teams[i].ExpectedGoals);
-                Teams.HomePoissonLimit[i] = double.Exp(-(teams[i].ExpectedGoals + Simulation.HomeAdvantage));
-            }
+            var poissonIndex = i * 2;
+            poisson[poissonIndex + 0] = double.Exp(-(teams[i].ExpectedGoals + Simulation.HomeAdvantage));
+            poisson[poissonIndex + 1] = double.Exp(-teams[i].ExpectedGoals);
         }
-    }
-}
 
-unsafe internal struct Teams
-{
-    public fixed double PoissonLimit[32];
-    public fixed double HomePoissonLimit[32];
-}
-
-internal static class Simulation
-{
-    internal const double HomeAdvantage = 0.25;
-    internal const int MaxNumberOfMatches = 32 * 32 * 2;
-
-    unsafe private struct Matches
-    {
-        public fixed byte Home[MaxNumberOfMatches];
-    }
-
-    unsafe public static void Run(in Input input)
-    {
-        Debug.Assert(input.TeamCount <= 32, "We store team indice as ID as a byte, and allocate fixed/static buffers");
-
-        var numberOfMatches = (input.TeamCount - 1) * input.TeamCount;
-        Span<byte> matches = stackalloc byte[MaxNumberOfMatches];
+        var matches = Matches;
         var matchIndex = 0;
-        var matchups = new HashSet<(byte Home, byte Away)>(numberOfMatches);
-        for (int i = 0; i < input.TeamCount; i++)
+        for (int i = 0; i < teams.Length; i++)
         {
-            for (int j = 0; j < input.TeamCount; j++)
+            for (int j = 0; j < teams.Length; j++)
             {
                 if (i == j)
                     continue;
 
-                if (matchups.Add(((byte)i, (byte)j)))
+                if (Matchups.Add(((byte)i, (byte)j)))
                 {
-                    matches[matchIndex] = (byte)i;
+                    matches[matchIndex + 0] = (byte)i;
                     matches[matchIndex + 1] = (byte)j;
                     matchIndex += 2;
                 }
             }
         }
 
-        var homeRng = Xoshiro256Plus.Create();
-        var awayRng = Xoshiro256Plus.Create();
+        TeamCount = teams.Length;
+        MatchCount = matchIndex;
+        Simulations = simulations;
+    }
 
-        Span<byte> scores = stackalloc byte[MaxNumberOfMatches];
+    public void Dispose()
+    {
+        NativeMemory.AlignedFree(_ptr);
+        Rng.Dispose();
+    }
+}
 
-        for (int simulation = 0; simulation < input.Simulations; simulation++)
+internal static class Simulation
+{
+    internal const double HomeAdvantage = 0.25;
+    internal const int MaxNumberOfTeams = 32;
+    internal const int MaxNumberOfMatches = 32 * 32;
+
+    unsafe public static void Run(ref State state)
+    {
+        ref var rng = ref state.Rng;
+        var scores = state.Scores;
+        var matches = state.Matches;
+        var poisson = state.Poisson;
+
+        var matchCount = state.MatchCount;
+
+        for (int simulation = 0; simulation < state.Simulations; simulation++)
         {
-            for (int i = 0; i < matchIndex; i += 2)
+            for (int i = 0; i < matchCount; i += (2 * 4))
             {
-                var homeId = matches[i];
-                var awayId = matches[i + 1];
-                var home = input.Teams.HomePoissonLimit[homeId];
-                var away = input.Teams.PoissonLimit[awayId];
+                var homeId1 = matches[i + 0];
+                var awayId1 = matches[i + 1];
+                var homePoissonIndex1 = homeId1 * 2;
+                var awayPoissonIndex1 = awayId1 * 2;
+                var home1 = poisson[homePoissonIndex1 + 0];
+                var away1 = poisson[awayPoissonIndex1 + 1];
+                Debug.Assert(home1 != 0, "Home poisson limit should not be 0");
+                Debug.Assert(away1 != 0, "Away poisson limit should not be 0");
 
-                var homeGoals = Simulate(home, ref homeRng);
-                var awayGoals = Simulate(away, ref awayRng);
+                var goals = Simulate(home1, ref rng);
 
-                scores[i] = homeGoals;
-                scores[i + 1] = awayGoals;
+                scores[i] = goals;
+                scores[i + 1] = goals;
             }
+        }
 
-            scores.Clear();
+        scores.Clear();
+    }
+
+    private static readonly Vector256<double> _adder = Vector256.Create(1d);
+    private static void Simulate(Vector256<double> poissonVec, ref Vector256<double> goals, ref Shishua rng)
+    {
+        Vector256<double> productVec = default;
+
+        while (true)
+        {
+            var condition = Avx2.CompareGreaterThanOrEqual(productVec, poissonVec);
+            goals = Avx2.Add(goals, Avx2.And(_adder, condition));
+
+            Vector256<double> nextProductVec = default;
+            rng.NextDoubles256(ref nextProductVec);
+            productVec = Avx2.Multiply(productVec, nextProductVec);
+
+            var sub = Avx2.Subtract(productVec, poissonVec);
+             // MoveMask extracts sign bits from the floats into the lower bits of the mask
+             // So if
+            var mask = Avx2.MoveMask(sub);
+            if (mask == 0x000F)
+                break;
         }
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private static byte Simulate(double poissonLimit, ref Xoshiro256Plus rng)
+    private static byte Simulate(double poisson, ref Shishua rng)
     {
         // Knuth's poisson algorithm
 
-        byte goals = 0;
+        byte goals1 = 0;
+        return goals1;
 
-        var product = rng.NextDouble();
-        while (product >= poissonLimit)
-        {
-            goals++;
-            product *= rng.NextDouble();
-        }
+        // var poissonVec = Vector256.Create(poissonLimit);
 
-        return goals;
+        // var product = rng.NextDouble();
+
+        // while (true)
+        // {
+        //     var product0 = product;
+        //     var product1 = product0 * rng.NextDouble();
+        //     var product2 = product1 * rng.NextDouble();
+        //     var product3 = product2 * rng.NextDouble();
+
+        //     var vec = Vector256.Create(product0, product1, product2, product3);
+        //     var res = Avx2.CompareGreaterThanOrEqual(vec, poissonVec);
+        //     var theseGoals = Vector256.Sum(res.AsInt64()) * -1;
+        //     goals += (byte)theseGoals;
+        //     if (theseGoals < 4)
+        //         return goals;
+
+        //     product = product3 * rng.NextDouble();
+        // }
+
+
+        // var product = rng.NextDouble();
+
+        // while (product >= poissonLimit)
+        // {
+        //     goals++;
+        //     product *= rng.NextDouble();
+        // }
     }
 
-    private static int NextPow2(int v)
+    internal static int NextPow2(int v)
     {
         v--;
         v |= v >> 1;
